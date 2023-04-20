6e0f7973ddb19189ebcab086b8a3cd4f7920836a
AkberRizvi123 <akber_rizvi16@hotmail.com>
2023-04-20 15:49:57 +0500

    Feat:
    - Added update profile and get profile routes and apis

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,7 @@
 
 const Business = require('../models/businessSchema');
 
+
 // Get all businesses
 const getAllBusiness=async (req, res) => {
     try {
@@ -32,4 +33,179 @@ const updateBusinessStatus = async(req,res)=>{
     res.status(500).json({ error: err.message });
   }
 }
-module.exports = {getAllBusiness, updateBusinessStatus}
+
+const profile = async (req, res) => {
+  try{
+    const adminId = req.userId;
+    const admin = await User.findById(adminId)
+    res.status(200).send(admin)
+  } catch(error){
+    res.status(500).send("Internal Server Error")
+  }
+
+}
+
+const updateAdminEmail = async (req, res) => {
+  try {
+    const adminId = req.userId;
+    const Email = req.body.Email;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const emailExist = await User.findOne({ Email: Email });
+    if (emailExist) {
+      return res
+        .status(400)
+        .json({ error: "This Email Address is already registered" });
+    }
+    const admin = await User.findByIdAndUpdate(
+      adminId,
+      { Email },
+      {
+        new: true,
+      }
+    );
+    res.json({ admin, message: "Email updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update email" });
+  }
+};
+
+const updateAdminPassword = async (req, res) => {
+  try {
+    const adminId = req.userId;
+    const { oldPassword, newPassword } = req.body;
+   
+    // Retrieve the admin from the database
+    const admin = await User.findById(adminId);
+    if (!admin) {
+      return res.status(404).json({ error: 'Admin not found' });
+    }
+
+    // Verify old password
+    const isPasswordValid = await bcrypt.compare(oldPassword, admin.Password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // Update password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    admin.Password = hashedPassword;
+    await admin.save();
+    return res.json({message: 'Password updated successfully',data:admin });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const updateAdminFirstName = async (req, res) => {
+  try {
+    const adminId = req.userId;
+    const FirstName = req.body.FirstName;
+    if (!FirstName) {
+      return res.json({ error: "Please enter first name" });
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const admin = await User.findByIdAndUpdate(
+      adminId,
+      { FirstName },
+      {
+        new: true,
+      }
+    );
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found" });
+    }
+    res.json({ admin, message: "First Name updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update first name" });
+  }
+};
+
+const updateAdminLastName = async (req, res) => {
+  try {
+    const adminId = req.userId;
+    const LastName = req.body.LastName;
+    if (!LastName) {
+      return res.json({ error: "Please Enter Last Name" });
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const admin = await User.findByIdAndUpdate(adminId, { LastName });
+    if (!admin) {
+      return res.status(404).json(
+        { error: "Admin not found" },
+        {
+          new: true,
+        }
+      );
+    }
+    res.json({ admin, message: "Last Name updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update last name" });
+  }
+};
+
+const updateAdminPhoneNo = async (req, res) => {
+  try {
+    const adminId = req.userId;
+    const PhoneNo = req.body.PhoneNo;
+    if (!PhoneNo) {
+      return res.json({ error: "Please enter phone number" });
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const admin = await User.findByIdAndUpdate(
+      adminId,
+      { PhoneNo },
+      {
+        new: true,
+      }
+    );
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found" });
+    }
+    res.json({ admin, message: "Phone number updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update phone number" });
+  }
+};
+
+const updateAdminProfilePicture = async (req, res) => {
+  try {
+    const adminId = req.userId;
+    const profile_picture = req.body.profile_picture;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const admin = await User.findByIdAndUpdate(
+      adminId,
+      { profile_picture },
+      {
+        new: true,
+      }
+    );
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found" });
+    }
+    res.json({ admin, message: "profile picture updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to update profile picture" });
+  }
+};
+
+module.exports = {getAllBusiness, updateBusinessStatus,updateAdminEmail,updateAdminPassword,updateAdminFirstName,updateAdminLastName,updateAdminPhoneNo,updateAdminProfilePicture,profile}
