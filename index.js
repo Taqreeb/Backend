090ece01dc29c41088eca2b08d7f95279d1e596e
AkberRizvi123 <akber_rizvi16@hotmail.com>
2023-04-29 18:16:06 +0500

    Added new port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const server = http.createServer(app);
 const env = require("dotenv");
 env.config({ path: 'config.env' });
-const PORT = process.env.port;
+const PORT = process.env.port || 5000;
 
 server.listen(PORT, () => {
     console.log("server up at localhost" + " " + PORT);
