e3dae68e0c0ad17fb8f9cb733fc8ff8fb7758ec9
AkberRizvi123 <akber_rizvi16@hotmail.com>
2023-04-30 00:36:38 +0500

    Configuring vercel

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const cookieParser=require('cookie-parser')
-const admin = require('./router/admin/routes')
-const user=require('./router/user/routes')
-const vendor=require('./router/vendor/routes')
-const auth=require('./router/auth/routes')
-const business=require('./router/business/routes')
-const review=require('./router/reviews/routes')
-const bodyParser = require('body-parser')
-const allowCors = require('./corshandler')
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const cookieParser=require('cookie-parser')
+// const admin = require('./router/admin/routes')
+// const user=require('./router/user/routes')
+// const vendor=require('./router/vendor/routes')
+// const auth=require('./router/auth/routes')
+// const business=require('./router/business/routes')
+// const review=require('./router/reviews/routes')
+// const bodyParser = require('body-parser')
+// const allowCors = require('./corshandler')
 
-require('./db/conn');
-app.use(cookieParser())
-app.use(allowCors);
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  secure: true
-}));
-// app.options('*', cors())
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// require('./db/conn');
+// app.use(cookieParser())
+// app.use(allowCors);
+// app.use(cors({
+//   origin: '*',
+//   credentials: true,
+//   secure: true
+// }));
+// // app.options('*', cors())
+// app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-//routes
-app.use('/auth', auth);
-app.use('/user', user);
-app.use('/user', review);
-app.use('/vendor', vendor);
-app.use('/vendor', business);
-app.use('/admin', admin);
+// //routes
+// app.use('/auth', auth);
+// app.use('/user', user);
+// app.use('/user', review);
+// app.use('/vendor', vendor);
+// app.use('/vendor', business);
+// app.use('/admin', admin);
 
-app.get('/',(req,res) => {
-  res.send("Welcome To Taqreeb");
-});
+// app.get('/',(req,res) => {
+//   res.send("Welcome To Taqreeb");
+// });
 
-app.use((req,res,next) => {
-  res.status(404).json( {
-      error: 'Bad Request: URL not found'
-  })
-})
+// app.use((req,res,next) => {
+//   res.status(404).json( {
+//       error: 'Bad Request: URL not found'
+//   })
+// })
 
-module.exports =app;
+// module.exports =app;
 
--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-//Import
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const cookieParser=require('cookie-parser')
+const admin = require('./router/admin/routes')
+const user=require('./router/user/routes')
+const vendor=require('./router/vendor/routes')
+const auth=require('./router/auth/routes')
+const business=require('./router/business/routes')
+const review=require('./router/reviews/routes')
+const bodyParser = require('body-parser')
+// const allowCors = require('./corshandler')
 const env = require("dotenv");
 env.config({ path: 'config.env' });
+require('./db/conn');
+app.use(cookieParser())
+// app.use(allowCors);
+app.use(cors());
+app.options('*', cors())
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+//routes
+app.use('/auth', auth);
+app.use('/user', user);
+app.use('/user', review);
+app.use('/vendor', vendor);
+app.use('/vendor', business);
+app.use('/admin', admin);
+
+app.get('/',(req,res) => {
+  res.send("Welcome To Taqreeb");
+});
+
+app.use((req,res,next) => {
+  res.status(404).json( {
+      error: 'Bad Request: URL not found'
+  })
+})
+
+
 const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("server up at localhost" + " " + PORT);
-  });
+});
