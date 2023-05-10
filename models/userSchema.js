f613eae83451defa202e6f92b0b048ba662094e2
AkberRizvi123 <akber_rizvi16@hotmail.com>
2023-05-11 04:15:24 +0500

    Added Email Verification through node mailer

--- models/userSchema.js
+++ models/userSchema.js
@@ -33,6 +33,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:"https://via.placeholder.com/150",
     },
+    verified:{
+      type:Boolean,
+      default:false,
+    }
   },
   { timestamps: true }
 );
